b40e23adbc451aa1b307507059a6cc445fe9250b
Dor Katzir <katzirdor@gmail.com>
2023-11-08 16:12:55 +0200

    added vite livewire plugin

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CodeBreaker</title>
-    @vite('resources/css/app.css')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
 </head>
 <body class="pt-8 bg-primary text-white print:bg-white print:text-black grid place-items-center h-full">
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,34 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import livewire from "@defstudio/vite-livewire-plugin";
 
 export default defineConfig({
+
+    server: {
+        host: "0.0.0.0",
+        hmr: {
+            host: "localhost",
+        },
+    },
     plugins: [
         laravel({
-            input: [
-                "resources/css/app.css",
-                "resources/js/app.js",
-            ],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
+        }),
+        {
+            name: "blade",
+            handleHotUpdate({ file, server }) {
+                if (file.endsWith(".blade.php") && !file.includes("livewire")) {
+                    server.ws.send({
+                        type: "full-reload",
+                        path: "*",
+                    });
+                }
+            },
+        },
+        livewire({
+            refresh: ["resources/css/app.css"],
         }),
     ],
 });
+

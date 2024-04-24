a02fc12ffeef85513f987a01905f4ded418e9435
Dor Katzir <katzirdor@gmail.com>
2024-04-24 22:33:07 +0300

    readme file

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CodeBreaker</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    {{-- <link rel="stylesheet" href="{{ asset('build/assets/app-5c6f9365.css') }}"> --}}
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
 </head>
 <body class="pt-8 bg-primary text-white print:bg-white print:text-black grid place-items-center h-full">
